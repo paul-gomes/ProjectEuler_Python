d7ae27720c389c88aa2a616659be1fddb49959b3
Anik Paul Gomes <gomesap29@gmail.com>
2018-09-18 00:25:53 -0500

    updated problem 3

--- Prob3_Largest_Prime_Factor/Prob3_Largest_Prime_Factor.py
+++ Prob3_Largest_Prime_Factor/Prob3_Largest_Prime_Factor.py
@@ -3,6 +3,21 @@
 #The prime factors of 13195 are 5, 7, 13 and 29.
 #What is the largest prime factor of the number 600851475143?
 
+
+# Prime number is any number that is only divisible by 1 and itself
+# To find the prime factorization, we need to break the number to its prime factors
+# "Factors" are the numbers you multiply together to get another number
+# Can we divide 147 exactly by 2?
+# 147 ÷ 2 = 73½
+# No it can't. The answer should be a whole number, and 73½ is not.
+# Let's try the next prime number, 3:
+# 147 ÷ 3 = 49
+# That worked, now we try factoring 49, and find that 7 is the smallest prime number that works:
+# 49 ÷ 7 = 7
+# And that is as far as we need to go, because all the factors are prime numbers.
+# 147 = 3 × 7 × 7
+
+
 def primeFactors(num):
     factor = []
     count= 2
